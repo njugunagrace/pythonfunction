4b93ece205b38c8a01bf5c087d17fa46383cfc57
njugunagrace <njugunagraceewambui@gamil.com>
2023-03-31 17:06:30 +0300

    initial commit

---
+++ functions/math.py
@@ -0,0 +1,25 @@
+def add(a,b):
+    answer= a+b
+    return answer
+
+def subtract(a,b):
+    answer = a-b
+    return answer  
+
+def divide(a,b):
+    answer = a/b
+    return answer
+
+  
+
+def multiply(a,b):
+    answer = a*b
+    return answer
+
+def remainder(c,d):
+    answ = c%d
+    return answ
+
+#  def remainder(a,b):
+#     answer= a%b
+#     return answer
